0259f7a26664e5800b2ed9938542379bd43295cb
root <root@Loaner01.providencepubliclibrary.org>
2023-03-31 16:12:23 -0400

    added login screen

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,32 @@
+import { Form } from "react-router-dom"
+
 const Home = (props) => {
-    return <h1>Home</h1>
+    return <>
+    <div>
+        <h1>Sign Up</h1>
+        <Form>
+            <label>
+                Email: <input type="text" name="email"/>
+            </label>
+            <label>
+                Password: <input type="password" name="password"/>
+            </label>
+            <button>Submit</button>
+        </Form>
+    </div>
+        <div>
+        <h1>Log In</h1>
+        <Form>
+            <label>
+                Email: <input type="text" name="email" required/>
+            </label>
+            <label>
+                Password: <input type="password" name="password" required/>
+            </label>
+            <button>Submit</button>
+        </Form>
+    </div>
+    </>
 }
 
 export default Home
